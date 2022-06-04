2b1e44b1c507f31f35fd03c583e28042b4492d75
Carl-from-OWCA <94492606+Carl-from-OWCA@users.noreply.github.com>
2022-06-04 18:13:14 -0400

    Started brainstorming

---
+++ solution_doomsday-fuel.py
@@ -0,0 +1,11 @@
+def solution(m: list[list[int]]) -> list[int]:
+    """
+    Okay so while I was showering, I realized that 
+    thi s problem is better represeted as a directed 
+    graph than something with dynamic programming. So 
+    I googled what "determine probablity of arriving at a 
+    node in a graph with loops" and it turns out this is
+    a problem based on Markov chains and there are a ton
+    of resources on it.
+    and """
+    print("Markov Chain time")
