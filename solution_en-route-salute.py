63bd832c91b4f347ee44488b57f9893693dd8269
Carl-from-OWCA <94492606+Carl-from-OWCA@users.noreply.github.com>
2022-06-04 14:08:12 -0400

    Very easy

---
+++ solution_en-route-salute.py
@@ -0,0 +1,8 @@
+def solution(s: str):
+    passes = 0
+    for i in range(0, len(s)):
+        if s[i] == '>':
+            for j in range(i + 1, len(s)):
+                if s[j] == '<':
+                    passes += 1
+    return 2 * passes
